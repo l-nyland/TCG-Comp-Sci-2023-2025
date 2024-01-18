3f80c346d9226e65aa18a1d32bd5ed9060dd450f
l-nyland <116548065+l-nyland@users.noreply.github.com>
2024-01-18 20:53:42 +0000

    Add files via upload

---
+++ Q6.py
@@ -0,0 +1,10 @@
+#Q6 page 199 
+def pond_Volume(length, width, depth):
+    volume = length * width * depth
+    print("Pond volume is: ", volume)
+     
+a = int(input("enter L: "))
+b = int(input("enter W: "))
+c = int(input("enter D: "))
+
+pond_Volume(a,b,c)
